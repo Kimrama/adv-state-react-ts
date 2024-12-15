bdb3203926f2b0f2bab6d53122692492f5cf6aad
Kimrama <kimrama0x@gmail.com>
2024-12-15 17:08:56 +0700

    start stop feature complete

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,45 @@
+import { useState, useEffect, useRef } from "react";
 import Container from "./UI/Container.tsx";
-import { Timer as TimerProps } from "../store/timer-context.tsx";
+import {
+    Timer as TimerProps,
+    useTimersContext,
+} from "../store/timer-context.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+    const [remainingTime, setRemainingTime] = useState(duration * 1000);
+    const interval = useRef<number | null>(null);
+    const { isRunning } = useTimersContext();
+
+    if (remainingTime <= 0 && interval.current) {
+        clearInterval(interval.current);
+    }
+    useEffect(() => {
+        let time: number;
+        if (isRunning) {
+            time = setInterval(() => {
+                setRemainingTime((prev) => {
+                    if (prev > 0) {
+                        return prev - 50;
+                    } else {
+                        return prev;
+                    }
+                });
+            }, 50);
+            interval.current = time;
+        } else if (interval.current) {
+            clearInterval(interval.current);
+        }
+
+        return () => clearInterval(time);
+    }, [isRunning]);
+
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
     return (
         <Container as="article">
             <h2>{name}</h2>
-            <p>{duration}</p>
+            <progress max={duration * 1000} value={remainingTime} />
+            <p>{formattedRemainingTime}</p>
         </Container>
     );
 }
